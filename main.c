94a6941b96e8d07b6d3893f3493ac77fead420e4
Hastro <valenatali0347@gmail.com>
2025-01-27 12:11:32 -0300

    switched to render sdl. Added decode and some execute instructions

--- main.c
+++ main.c
@@ -15,6 +15,8 @@ uint8_t display[64 * 32] = {0};
 
 uint16_t fetch(Memory *mem);
 
+void decode(uint16_t instruction, uint8_t *opcode, uint8_t *X, uint8_t *Y, uint16_t *NNN, uint8_t *NN, uint8_t *N);
+
 int main(int argc, char *argv[])
 {
     if (SDL_Init(SDL_INIT_VIDEO) < 0)
@@ -33,40 +35,105 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    SDL_Surface *window_surface = SDL_GetWindowSurface(window);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    if (!window_surface)
+    if (!renderer)
     {
-        SDL_Log("Unable to obtain window surface", SDL_GetError());
+        printf("Error al crear el renderer: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
     }
 
-    SDL_Event e;
-    while (SDL_PollEvent(&e) > 0)
-    {
-        SDL_UpdateWindowSurface(window);
-    }
+    int running = 1;
+    SDL_Event event;
 
-    bool keep_window_open = true;
-    while (keep_window_open)
+    Memory *mem = {0};
+    mem->pc = 0x200;
+
+    uint16_t instruction;
+
+    uint8_t opcode;
+    uint8_t X;
+    uint8_t Y;
+    uint16_t NNN;
+    uint8_t NN;
+    uint8_t N;
+
+    while (running)
     {
-        SDL_Event e;
-        while (SDL_PollEvent(&e) > 0)
+        while (SDL_PollEvent(&event))
         {
-            switch (e.type)
+            if (event.type == SDL_QUIT)
             {
-            case SDL_QUIT:
-                keep_window_open = false;
+                running = 0;
+            }
+        }
+
+        instruction = fetch(mem);
+        decode(instruction, opcode, X, Y, NNN, NN, N);
+
+        switch (opcode)
+        {
+        case 0x0:
+            switch (NNN)
+            {
+            case 0x0E0:
+                SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+                SDL_RenderClear(renderer);
+                break;
+
+            default:
                 break;
             }
+            break;
 
-            SDL_UpdateWindowSurface(window);
+        case 0x1:
+            mem->pc = NNN;
+            break;
+
+        case 0x6:
+            mem->V[X] = NN;
+            break;
+
+        case 0x7:
+            mem->V[X] += NN;
+            break;
+
+        case 0xA:
+            mem->I = NNN;
+            break;
+
+        case 0xD:
+            break;
+
+        default:
+            break;
         }
+
+        SDL_RenderPresent(renderer);
     }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return 0;
 }
 
 uint16_t fetch(Memory *mem)
 {
-    uint16_t instruction = (mem->ram[mem->pc] << 8) | (mem->ram[mem->pc + 1])
+    uint16_t instruction = (mem->ram[mem->pc] << 8) | (mem->ram[mem->pc + 1]);
+    mem->pc += 2;
+    return instruction;
+}
+
+void decode(uint16_t instruction, uint8_t *opcode, uint8_t *X, uint8_t *Y, uint16_t *NNN, uint8_t *NN, uint8_t *N)
+{
+    *opcode = instruction >> 12;
+    *X = (instruction & 0x0F00) >> 8;
+    *Y = (instruction & 0x00F0) >> 4;
+    *NNN = instruction & 0x0FFF;
+    *NN = instruction & 0x00FF;
+    *N = instruction & 0x000F;
 }
